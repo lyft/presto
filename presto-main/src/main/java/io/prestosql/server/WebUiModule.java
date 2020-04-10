3954092726d4c853a36a14d64900347e256aa472
James Taylor <jamestaylor@lyft.com>
2020-04-09 17:02:23 -0700

    Disable authentication for web-ui

--- presto-main/src/main/java/io/prestosql/server/WebUiModule.java
+++ presto-main/src/main/java/io/prestosql/server/WebUiModule.java
@@ -19,14 +19,27 @@ import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.prestosql.server.ui.ClusterResource;
 import io.prestosql.server.ui.ClusterStatsResource;
 import io.prestosql.server.ui.DisabledWebUiAuthenticationManager;
-import io.prestosql.server.ui.FormWebUiAuthenticationManager;
 import io.prestosql.server.ui.UiQueryResource;
 import io.prestosql.server.ui.WebUiAuthenticationManager;
 import io.prestosql.server.ui.WebUiConfig;
+import io.prestosql.spi.security.BasicPrincipal;
+import io.prestosql.spi.security.Identity;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.security.Principal;
 
 import static io.airlift.configuration.ConfigBinder.configBinder;
 import static io.airlift.http.server.HttpServerBinder.httpServerBinder;
 import static io.airlift.jaxrs.JaxrsBinder.jaxrsBinder;
+import static io.prestosql.server.HttpRequestSessionContext.AUTHENTICATED_IDENTITY;
+import static java.util.Objects.requireNonNull;
 
 public class WebUiModule
         extends AbstractConfigurationAwareModule
@@ -39,13 +52,41 @@ public class WebUiModule
         configBinder(binder).bindConfig(WebUiConfig.class);
 
         if (buildConfigObject(WebUiConfig.class).isEnabled()) {
-            binder.bind(WebUiAuthenticationManager.class).to(FormWebUiAuthenticationManager.class).in(Scopes.SINGLETON);
+            binder.bind(WebUiAuthenticationManager.class).to(FixedUserWebUiAuthenticationManager.class).in(Scopes.SINGLETON);
             jaxrsBinder(binder).bind(ClusterResource.class);
             jaxrsBinder(binder).bind(ClusterStatsResource.class);
             jaxrsBinder(binder).bind(UiQueryResource.class);
         }
         else {
             binder.bind(WebUiAuthenticationManager.class).to(DisabledWebUiAuthenticationManager.class).in(Scopes.SINGLETON);
+        }
+    }
+
+    public static class FixedUserWebUiAuthenticationManager
+            implements WebUiAuthenticationManager
+    {
+        @Override
+        public void handleUiRequest(HttpServletRequest request, HttpServletResponse response, FilterChain nextFilter)
+                throws IOException, ServletException
+        {
+            nextFilter.doFilter(withUsername(request, "web-ui"), response);
+        }
+
+        private static ServletRequest withUsername(HttpServletRequest request, String username)
+        {
+            requireNonNull(username, "username is null");
+            BasicPrincipal principal = new BasicPrincipal(username);
+            request.setAttribute(AUTHENTICATED_IDENTITY, Identity.forUser(username)
+                    .withPrincipal(principal)
+                    .build());
+            return new HttpServletRequestWrapper(request)
+            {
+                @Override
+                public Principal getUserPrincipal()
+                {
+                    return principal;
+                }
+            };
         }
     }
 }
