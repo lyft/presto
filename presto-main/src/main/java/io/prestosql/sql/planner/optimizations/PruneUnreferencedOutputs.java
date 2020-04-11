kasiafi/trino

Support cross join outputs pruning in PruneUnreferencedOutputs

presto-main/src/main/java/io/prestosql/sql/planner/optimizations/PruneUnreferencedOutputs.java
@@ -212,21 +212,10 @@ public class PruneUnreferencedOutputs
             PlanNode left = context.rewrite(node.getLeft(), leftInputs);
             PlanNode right = context.rewrite(node.getRight(), rightInputs);
 
-            List<Symbol> outputSymbols;
-            if (node.isCrossJoin()) {
-                // do not prune nested joins output since it is not supported
-                // TODO: remove this "if" branch when output symbols selection is supported by nested loop join
-                outputSymbols = ImmutableList.<Symbol>builder()
-                        .addAll(left.getOutputSymbols())
-                        .addAll(right.getOutputSymbols())
-                        .build();
-            }
-            else {
-                outputSymbols = node.getOutputSymbols().stream()
-                        .filter(context.get()::contains)
-                        .distinct()
-                        .collect(toImmutableList());
-            }
+            List<Symbol> outputSymbols = node.getOutputSymbols().stream()
+                    .filter(context.get()::contains)
+                    .distinct()
+                    .collect(toImmutableList());
 
             return new JoinNode(
                     node.getId(),

lyft/presto

presto-main/src/main/java/io/prestosql/sql/planner/optimizations/PruneUnreferencedOutputs.java
@@ -212,24 +212,14 @@ public class PruneUnreferencedOutputs
             PlanNode left = context.rewrite(node.getLeft(), leftInputs);
             PlanNode right = context.rewrite(node.getRight(), rightInputs);
 
-            List<Symbol> leftOutputSymbols;
-            List<Symbol> rightOutputSymbols;
-
-            // do not prune cross join output since it is not supported
-            if (node.isCrossJoin()) {
-                leftOutputSymbols = left.getOutputSymbols();
-                rightOutputSymbols = right.getOutputSymbols();
-            }
-            else {
-                leftOutputSymbols = node.getLeftOutputSymbols().stream()
-                        .filter(context.get()::contains)
-                        .distinct()
-                        .collect(toImmutableList());
-                rightOutputSymbols = node.getRightOutputSymbols().stream()
-                        .filter(context.get()::contains)
-                        .distinct()
-                        .collect(toImmutableList());
-            }
+            List<Symbol> leftOutputSymbols = node.getLeftOutputSymbols().stream()
+                    .filter(context.get()::contains)
+                    .distinct()
+                    .collect(toImmutableList());
+            List<Symbol> rightOutputSymbols = node.getRightOutputSymbols().stream()
+                    .filter(context.get()::contains)
+                    .distinct()
+                    .collect(toImmutableList());
 
             return new JoinNode(
                     node.getId(),